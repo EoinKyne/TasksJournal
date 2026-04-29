2cf670018429be7031abe01d26df50896df210ac
ekyne <eoin_kyne@yahoo.com>
2026-04-29 16:22:04 +0100

    update providers

--- infra/environments/dev/providers.tf
+++ infra/environments/dev/providers.tf
@@ -1,18 +1,9 @@
 provider "kubernetes" {
-  config_path = "D:\\minikube\\.kube\\config"
-  config_context = "minikube"
-  #host                   = "https://127.0.0.1:53547"
-  #client_certificate     = "D:\\minikube\\.minikube\\profiles\\minikube\\client.crt"
-  #client_key             = "D:\\minikube\\.minikube\\profiles\\minikube\\client.key"
-  #cluster_ca_certificate = "D:\\minikube\\.minikube\\ca.crt"
+
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "D:\\minikube\\.kube\\config"
-    #host                   = "https://127.0.0.1:53547"
-    #client_certificate     = "D:\\minikube\\.minikube\\profiles\\minikube\\client.crt"
-    #client_key             = "D:\\minikube\\.minikube\\profiles\\minikube\\client.key"
-    #cluster_ca_certificate = "D:\\minikube\\.minikube\\ca.crt"
+    
   }
 }
