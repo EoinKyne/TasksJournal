12af7f686f50a34b1dcf40128e579094ef4db743
EoinKyne <Eoin_kyne@yahoo.com>
2026-04-30 14:12:15 +0100

    Feature add linter to build tests workflow (#23)
    * rename and update run-test for linters
    
    * update workflow
    
    * update workflow
    
    * updates forp python linter
    
    * update for python linter
    
    * update to use repo secrets
    
    * undo update to use repo secrets

--- journal_app/tests/test_journal_entry_api.py
+++ journal_app/tests/test_journal_entry_api.py
@@ -16,10 +16,12 @@ class JournalEntryAPITest(APITestCase):
         response = self.client.post(url, data, format='json')
 
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(JournalEntry.objects.first().title, 'Test Case entry')
+        self.assertEqual(JournalEntry.objects.first().title,
+                         'Test Case entry')
 
     def test_get_entries(self):
-        JournalEntry.objects.create(title='Test', description='Description')
+        JournalEntry.objects.create(
+            title='Test', description='Description')
         url = reverse('journalentry-list')
         response = self.client.get(url)
 
@@ -27,14 +29,17 @@ class JournalEntryAPITest(APITestCase):
         self.assertEqual(len(response.data), 1)
 
     def test_get_entry_by_id(self):
-        entry = JournalEntry.objects.create(title='Test', description='Description')
+        entry = JournalEntry.objects.create(
+            title='Test', description='Description')
 
-        response = self.client.get(reverse('journalentry-detail', args=[entry.id]))
+        response = self.client.get(
+            reverse('journalentry-detail', args=[entry.id]))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["id"], entry.id)
 
     def test_update_entry(self):
-        entry = JournalEntry.objects.create(title='Test', description='Description')
+        entry = JournalEntry.objects.create(
+            title='Test', description='Description')
 
         url = reverse('journalentry-detail', args=[entry.id])
 
@@ -50,9 +55,11 @@ class JournalEntryAPITest(APITestCase):
         self.assertEqual(entry.title, "Update test title")
 
     def test_delete_entry(self):
-        entry = JournalEntry.objects.create(title='Test', description='Description')
+        entry = JournalEntry.objects.create(
+            title='Test', description='Description')
 
-        response = self.client.delete(reverse('journalentry-detail', args=[entry.id]))
+        response = self.client.delete(
+            reverse('journalentry-detail', args=[entry.id]))
 
         self.assertEqual(response.status_code, 204)
         self.assertEqual(JournalEntry.objects.count(), 0)
